d5784997ef0a0404caf9bc5dfa143d874c338e17
Kyrylo Maksymenko <saklar13@gmail.com>
2019-06-22 13:39:50 +0300

    black

--- tests/networking/alcatel/base_test.py
+++ tests/networking/alcatel/base_test.py
@@ -2,15 +2,19 @@ import re
 from unittest import TestCase
 
 from cloudshell.devices.driver_helper import get_cli
-from cloudshell.devices.standards.networking.configuration_attributes_structure import \
-    create_networking_resource_from_context
-from cloudshell.shell.core.driver_context import ResourceCommandContext, ResourceContextDetails
-from mock import create_autospec, MagicMock
+from cloudshell.devices.standards.networking.configuration_attributes_structure import (
+    create_networking_resource_from_context,
+)
+from cloudshell.shell.core.driver_context import (
+    ResourceCommandContext,
+    ResourceContextDetails,
+)
+from mock import MagicMock, create_autospec
 
 from cloudshell.networking.alcatel.cli.alcatel_cli_handler import AlcatelCliHandler
 
-DEFAULT_PROMPT = 'A:ALU#'
-ENABLE_PASSWORD = 'enable password'
+DEFAULT_PROMPT = "A:ALU#"
+ENABLE_PASSWORD = "enable password"
 
 
 class CliEmulator(object):
@@ -19,9 +23,13 @@ class CliEmulator(object):
         self.previous_resp = None
 
         self.actions = [
-            ['^$', DEFAULT_PROMPT, None],
-            ['^environment no more$', DEFAULT_PROMPT, None],
-            ['^enable-admin$', 'MINOR: CLI Already in admin mode.\n{}'.format(DEFAULT_PROMPT), None],
+            ["^$", DEFAULT_PROMPT, None],
+            ["^environment no more$", DEFAULT_PROMPT, None],
+            [
+                "^enable-admin$",
+                "MINOR: CLI Already in admin mode.\n{}".format(DEFAULT_PROMPT),
+                None,
+            ],
         ]
 
         if actions:
@@ -46,8 +54,11 @@ class CliEmulator(object):
 
                     return response
                 else:
-                    raise ValueError('"{}" command called more times than needed'.format(
-                        self.command))
+                    raise ValueError(
+                        '"{}" command called more times than needed'.format(
+                            self.command
+                        )
+                    )
 
         raise KeyError('Doesn\'t find "{}" command'.format(self.command))
 
@@ -60,45 +71,50 @@ class CliEmulator(object):
     def check_calls(self):
         for command, response, calls in self.actions:
             if calls is not None and calls != 0:
-                raise ValueError('{} not executed needed times ({} left)'.format(command, calls))
+                raise ValueError(
+                    "{} not executed needed times ({} left)".format(command, calls)
+                )
 
 
 class BaseAlcatelTestCase(TestCase):
-    SHELL_NAME = ''
+    SHELL_NAME = ""
 
     def create_context(self, attrs=None):
         context = create_autospec(ResourceCommandContext)
         context.resource = create_autospec(ResourceContextDetails)
-        context.resource.name = 'Alcatel'
-        context.resource.fullname = 'Alcatel'
-        context.resource.family = 'CS_Router'
-        context.resource.address = 'host'
+        context.resource.name = "Alcatel"
+        context.resource.fullname = "Alcatel"
+        context.resource.family = "CS_Router"
+        context.resource.address = "host"
         context.resource.attributes = {}
 
         attributes = {
-            'User': 'user',
-            'Password': 'password',
-            'Enable Password': ENABLE_PASSWORD,
-            'host': 'host',
-            'CLI Connection Type': 'ssh',
-            'Sessions Concurrency Limit': '1',
+            "User": "user",
+            "Password": "password",
+            "Enable Password": ENABLE_PASSWORD,
+            "host": "host",
+            "CLI Connection Type": "ssh",
+            "Sessions Concurrency Limit": "1",
         }
         attributes.update(attrs or {})
 
         for key, val in attributes.items():
-            context.resource.attributes['{}{}'.format(self.SHELL_NAME, key)] = val
+            context.resource.attributes["{}{}".format(self.SHELL_NAME, key)] = val
 
         return context
 
     def _setUp(self, attrs=None):
         self.resource_config = create_networking_resource_from_context(
-            self.SHELL_NAME, ['TiMOS'], self.create_context(attrs))
+            self.SHELL_NAME, ["TiMOS"], self.create_context(attrs)
+        )
         self._cli = get_cli(int(self.resource_config.sessions_concurrency_limit))
 
         self.logger = MagicMock()
         self.api = MagicMock(DecryptPassword=lambda password: MagicMock(Value=password))
 
-        self.cli_handler = AlcatelCliHandler(self._cli, self.resource_config, self.logger, self.api)
+        self.cli_handler = AlcatelCliHandler(
+            self._cli, self.resource_config, self.logger, self.api
+        )
 
     def tearDown(self):
         self._cli._session_pool._session_manager._existing_sessions = []
