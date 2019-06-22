d56f82734458739e8b532e11835afb8675326eea
Kyrylo Maksymenko <saklar13@gmail.com>
2019-06-22 13:00:03 +0300

    black

--- cloudshell/networking/alcatel/command_templates/port_template.py
+++ cloudshell/networking/alcatel/command_templates/port_template.py
@@ -1,13 +1,14 @@
 from cloudshell.cli.command_template.command_template import CommandTemplate
 
-
 SET_PORT_MODE = CommandTemplate(
-    'configure port {port} ethernet mode {mode}',
-    error_map={
-        'A SAP exists on port': Exception('Can\'t change port mode')})
-SET_PORT_ENCAP_TYPE = CommandTemplate('configure port {port} ethernet encap-type {encap_type}')
+    "configure port {port} ethernet mode {mode}",
+    error_map={"A SAP exists on port": Exception("Can't change port mode")},
+)
+SET_PORT_ENCAP_TYPE = CommandTemplate(
+    "configure port {port} ethernet encap-type {encap_type}"
+)
 
-SHUTDOWN = CommandTemplate('configure port {port} shutdown')
-NO_SHUTDOWN = CommandTemplate('configure port {port} no shutdown')
+SHUTDOWN = CommandTemplate("configure port {port} shutdown")
+NO_SHUTDOWN = CommandTemplate("configure port {port} no shutdown")
 
-SHOW_PORT_ETHERNET = CommandTemplate('show port {port} ethernet')
+SHOW_PORT_ETHERNET = CommandTemplate("show port {port} ethernet")
