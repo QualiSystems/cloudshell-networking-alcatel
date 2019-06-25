33b504d284c78f2c95c4d7a89f45c6aac5cd0d26
Kyrylo Maksymenko <saklar13@gmail.com>
2019-06-25 15:31:00 +0300

    flake8-docstring

--- cloudshell/networking/alcatel/command_actions/port_actions.py
+++ cloudshell/networking/alcatel/command_actions/port_actions.py
@@ -9,27 +9,25 @@ from cloudshell.networking.alcatel.command_templates import port_template
 
 class PortActions(object):
     def __init__(self, cli_service, logger, port_name):
-        """Port actions
+        """Port actions.
 
         :param cli_service: enable mode cli_service
         :type cli_service: cloudshell.cli.cli_service.CliService
         :param logger:
         :param str port_name:
         """
-
         self._cli_service = cli_service
         self._logger = logger
         self.port_name = self._get_port_name(port_name)
 
     def _get_port_name(self, port_name):
-        """Get port name from port resource full address
+        """Get port name from port resource full address.
 
         :param str port_name: port resource full address
             e.g. alcatel/Chassis 1/Module 1/SubModule 1/Port 1-1-14
         :return: port name (1/1/14)
         :rtype: str
         """
-
         if not port_name:
             err_msg = "Failed to get port name."
             self._logger.error(err_msg)
@@ -45,22 +43,20 @@ class PortActions(object):
         return port
 
     def set_port_mode(self, qnq):
-        """Set port mode
+        """Set port mode.
 
         :param bool qnq:
         """
-
         mode = "hybrid" if qnq else "network"
         CommandTemplateExecutor(
             self._cli_service, port_template.SET_PORT_MODE
         ).execute_command(port=self.port_name, mode=mode)
 
     def set_port_encap_type(self, qnq):
-        """Set port encap type
+        """Set port encap type.
 
         :param bool qnq:
         """
-
         encap_type = "qinq" if qnq else "dot1q"
         CommandTemplateExecutor(
             self._cli_service, port_template.SET_PORT_ENCAP_TYPE
